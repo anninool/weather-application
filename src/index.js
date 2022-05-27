bd208f8a53b532a27c2e56be912fc0813b76a285
anninool <anni.nool03@gmail.com>
2022-05-27 17:22:31 +0100

    Changed weather icon layout using css

--- src/index.js
+++ src/index.js
@@ -50,7 +50,7 @@ function displayForecast(response) {
             forecastDay.weather[0].icon
           }@2x.png"
           alt=""
-          width="42"
+          width="52"
         />
                     <div class="weather-forecast-day">
                       ${formatDay(forecastDay.dt)}
